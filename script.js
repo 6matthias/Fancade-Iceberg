f88cd85f3bec609045bb3f8dc082ec14f27a6891
6matthias <69079462+6matthias@users.noreply.github.com>
2023-05-16 01:18:47 +0200

    Added functionality that generates text from data

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
 
     for(i = 0; i<10; i++) {
-        let tier = `tier${i+1}`;
-        $("#tiers").append(`<div class="tier" id="${tier}"></div>`) 
-        $(`#${tier}`).append("<p>hello</p>")
-    }
+        let tier = `tier${i}`;
+        $("#tiers").append(`<div class="tier" id="${tier}"></div>`)
+    }  // Create the divs that are going to hold the entries above the iceberg image
 
-    $("#iceberg").append(`<img src="assets/icebergImage.png" id="icebergImage">`)
+    $("#iceberg").append(`<img src="assets/icebergImage.png" id="icebergImage">`) //Add image
 
     var tmpImg = new Image();
     tmpImg.src = "assets/icebergImage.png"
 
-    $(tmpImg).one('load',function(){
-        $("#webContainer").css("width", tmpImg.width + "px")
-      });
+    $(tmpImg).one('load',function(){ //wait for image to load
+        $("#webContainer").css("max-width", tmpImg.width + "px") //Get iceberg image size and set the website container to be it's width size
+    });
+
+    $.getJSON('https://raw.githubusercontent.com/6matthias/Fancade-Iceberg/main/assets/data.json', function(data) {
+        for (const [key, value] of Object.entries(data)) {
+            $(`#tier`+data[key].tier).append(`<p class="entry">${key}</p>`)
+        }
+    });
+
 })
