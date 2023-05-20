5dcdcd796a643dde04444613db4b38eb28cc1195
6matthias <69079462+6matthias@users.noreply.github.com>
2023-05-20 17:22:29 +0200

    Force fit entries

--- script.js
+++ script.js
@@ -10,14 +10,16 @@ $(document).ready(function() {
     var tmpImg = new Image();
     tmpImg.src = "assets/icebergImage.png"
 
-    $(tmpImg).one('load',function(){ //wait for image to load
-        $("#webContainer").css("max-width", tmpImg.width + "px") //Get iceberg image size and set the website container to be it's width size
-    });
-
     $.getJSON('https://raw.githubusercontent.com/6matthias/Fancade-Iceberg/main/assets/data.json', function(data) {
         for (const [key, value] of Object.entries(data)) {
             $(`#tier`+data[key].tier).append(`<p class="entry">${key}</p>`)
         }
+
+        $(tmpImg).one('load',function(){ //wait for image to load
+            $("#webContainer").css("max-width", tmpImg.width + "px") //Get iceberg image size and set the website container to be it's width size
+            $(".tier").fitText(4)
+        });
+
     });
 
 })
